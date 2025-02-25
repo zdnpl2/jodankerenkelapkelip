0fa38a1cc982ab12ed6d50ba8de99b07d2d4027e
zdnpl2 <90134548+zdnpl2@users.noreply.github.com>
2025-02-25 08:59:21 +0700

    Add files via upload

---
+++ VIRUSJODAN99.bat
@@ -0,0 +1,62 @@
+@echo off
+title JODAN NINUNINU
+color 0e
+
+:A
+:top
+cls
+echo.
+echo.
+echo.
+echo. JODAN GANTENG BANGET
+echo.
+echo.
+echo.
+color 1a
+ping localhost -n 0 >nul
+color 2b
+ping localhost -n 0 >nul
+color 3c
+ping localhost -n 0 >nul
+color 4d
+ping localhost -n 0 >nul
+color 5e
+ping localhost -n 0 >nul
+color 6f
+ping localhost -n 0 >nul
+color 7a
+ping localhost -n 0 >nul
+color 8b
+ping localhost -n 0 >nul
+color 9c
+ping localhost -n 0 >nul
+goto time
+
+:time
+cls
+echo.
+echo.
+echo.
+echo. ADUH SAKIT MATA
+echo.
+echo.
+echo.
+color 1a
+ping localhost -n 0 >nul
+color 2b
+ping localhost -n 0 >nul
+color 3c
+ping localhost -n 0 >nul
+color 4d
+ping localhost -n 0 >nul
+color 5e
+ping localhost -n 0 >nul
+color 6f
+ping localhost -n 0 >nul
+color 7a
+ping localhost -n 0 >nul
+color 8b
+ping localhost -n 0 >nul
+color 9c
+ping localhost -n 0 >nul
+goto top
